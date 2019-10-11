28b2ac56cb58b8dc698d90df574084f120fce35c
Precious <xprecious1999x@gmail.com>
2019-10-11 16:01:38 -0500

    Dotify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,8 +15,8 @@
 #
 
 PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/aosp_judypn.mk
+    $(LOCAL_DIR)/dot_judypn.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_judypn-userdebug \
-    aosp_judypn-eng
+    dot_judypn-userdebug \
+    dot_judypn-eng
